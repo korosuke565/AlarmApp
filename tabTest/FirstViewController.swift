20dece1993a6b017d3ffc9a7ce18c5ad81819829
korosuke565 <t12619tt@sfc.keio.ac.jp>
2016-02-19 10:14:41 +0900

    Initial Commit

---
+++ tabTest/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  tabTest
+//
+//  Created by TsuyoshiTonobe on 2016/02/19.
+//  Copyright © 2016年 TsuyoshiTonobe. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ tabTest/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  tabTest
+//
+//  Created by TsuyoshiTonobe on 2016/02/19.
+//  Copyright © 2016年 TsuyoshiTonobe. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
